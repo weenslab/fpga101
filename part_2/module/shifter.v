84d7b94fa2a9cb832d29ac1bad630892a62d0a45
weenslab <166514783+weenslab@users.noreply.github.com>
2024-05-24 20:23:34 +0700

    Update shifter.v

--- part_2/module/shifter.v
+++ part_2/module/shifter.v
@@ -13,14 +13,14 @@ module shifter
 //    always @(*) // Wildcard, produce the same result
     begin
         case ({dir, amt})
-            3'b000: y_tmp <= {a[6:0], 1'b0};
-            3'b001: y_tmp <= {a[5:0], 2'b00};
-            3'b010: y_tmp <= {a[4:0], {3{1'b0}}}; // Replicate bit
-            3'b011: y_tmp <= {a[3:0], 4'b0000};
-            3'b100: y_tmp <= {1'b0, a[7:1]};
-            3'b101: y_tmp <= {2'b00, a[7:2]};
-            3'b110: y_tmp <= {3'b000, a[7:3]};
-            3'b111: y_tmp <= {4'b0000, a[7:4]};
+            3'b000: y_tmp = {a[6:0], 1'b0};
+            3'b001: y_tmp = {a[5:0], 2'b00};
+            3'b010: y_tmp = {a[4:0], {3{1'b0}}}; // Replicate bit
+            3'b011: y_tmp = {a[3:0], 4'b0000};
+            3'b100: y_tmp = {1'b0, a[7:1]};
+            3'b101: y_tmp = {2'b00, a[7:2]};
+            3'b110: y_tmp = {3'b000, a[7:3]};
+            3'b111: y_tmp = {4'b0000, a[7:4]};
             default: y_tmp <= 8'h00;
         endcase
     end
